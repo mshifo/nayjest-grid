2e4aee167d511f266486555cf5d4a69144a1a659
mahmoud <mahmoud@durarmail.com>
2018-04-13 21:29:26 +0400

    first commit

---
+++ app/Http/Controllers/tablecontroller.php
@@ -0,0 +1,273 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Grids;
+use HTML;
+use Illuminate\Support\Facades\Config;
+use Nayjest\Grids\Components\Base\RenderableRegistry;
+use Nayjest\Grids\Components\ColumnHeadersRow;
+use Nayjest\Grids\Components\ColumnsHider;
+use Nayjest\Grids\Components\CsvExport;
+use Nayjest\Grids\Components\ExcelExport;
+use Nayjest\Grids\Components\Filters\DateRangePicker;
+use Nayjest\Grids\Components\FiltersRow;
+use Nayjest\Grids\Components\HtmlTag;
+use Nayjest\Grids\Components\Laravel5\Pager;
+use Nayjest\Grids\Components\OneCellRow;
+use Nayjest\Grids\Components\RecordsPerPage;
+use Nayjest\Grids\Components\RenderFunc;
+use Nayjest\Grids\Components\ShowingRecords;
+use Nayjest\Grids\Components\TFoot;
+use Nayjest\Grids\Components\THead;
+use Nayjest\Grids\Components\TotalsRow;
+use Nayjest\Grids\DbalDataProvider;
+use Nayjest\Grids\EloquentDataProvider;
+use Nayjest\Grids\FieldConfig;
+use Nayjest\Grids\FilterConfig;
+use Nayjest\Grids\Grid;
+use Nayjest\Grids\GridConfig;
+
+class tablecontroller extends Controller {
+    /*
+     * basic example
+     */
+
+    public function showtable() {
+        $query = DB::getDoctrineConnection()->createQueryBuilder();
+        $query
+                ->select([
+                    '*'
+                ])
+                ->from('users');
+        $cfg = (new GridConfig())
+                ->setDataProvider(
+                        new DbalDataProvider($query)
+                )
+                ->setPageSize(8)
+                ->setColumns([
+            (new FieldConfig('id'))->setSortable(true)->addFilter((new FilterConfig)->setOperator(FilterConfig::OPERATOR_LIKE)),
+            (new FieldConfig('name'))->setSortable(true)->addFilter((new FilterConfig)->setOperator(FilterConfig::OPERATOR_LIKE)),
+            (new FieldConfig('email'))->setSortable(true)->addFilter((new FilterConfig)->setOperator(FilterConfig::OPERATOR_LIKE)),
+            (new FieldConfig('created_at'))->setSortable(true)->addFilter((new FilterConfig)->setOperator(FilterConfig::OPERATOR_LIKE)),
+            (new FieldConfig('updated_at'))->setSortable(true)->addFilter((new FilterConfig)->setOperator(FilterConfig::OPERATOR_LIKE)),
+//            (new FieldConfig('interest'))->setSortable(true)->addFilter((new FilterConfig)->setOperator(FilterConfig::OPERATOR_LIKE)),
+//            (new FieldConfig('mobile'))->setLabel('Phone Number')->setSortable(true)->addFilter((new FilterConfig)->setOperator(FilterConfig::OPERATOR_LIKE)),
+        ]);
+        $grid = new Grid($cfg);
+        /* $grid = Grids::make($cfg); */
+        return view('table', compact('grid'));
+    }
+
+    public function getExample1() {
+        $cfg = [
+            'src' => 'App\User',
+            'columns' => [
+                'id',
+                'name',
+                'email',
+            ]
+        ];
+        $grid = Grids::make($cfg);
+        $text = "<h1>Basic grid example</h1>";
+        return view('table', compact('grid', 'text'));
+    }
+
+   public function getExample4()
+    {
+        $grid = new Grid(
+            (new GridConfig)
+                ->setDataProvider(
+                    new EloquentDataProvider(User::query())
+                )
+                ->setName('example_grid4')
+                ->setPageSize(15)
+                ->setColumns([
+                    (new FieldConfig)
+                        ->setName('id')
+                        ->setLabel('ID')
+                        ->setSortable(true)
+                        ->setSorting(Grid::SORT_ASC)
+                    ,
+                    (new FieldConfig)
+                        ->setName('name')
+                        ->setLabel('Name')
+                        ->setCallback(function ($val) {
+                            return "<span class='glyphicon glyphicon-user'></span>{$val}";
+                        })
+                        ->setSortable(true)
+                        ->addFilter(
+                            (new FilterConfig)
+                                ->setOperator(FilterConfig::OPERATOR_LIKE)
+                        )
+                    ,
+                    (new FieldConfig)
+                        ->setName('email')
+                        ->setLabel('Email')
+                        ->setSortable(true)
+                        ->setCallback(function ($val) {
+                            $icon = '<span class="glyphicon glyphicon-envelope"></span>&nbsp;';
+                            return
+                                '<small>'
+                                . $icon
+                                . HTML::link("mailto:$val", $val)
+                                . '</small>';
+                        })
+                        ->addFilter(
+                            (new FilterConfig)
+                                ->setOperator(FilterConfig::OPERATOR_LIKE)
+                        )
+                    ,
+                    (new FieldConfig)
+                        ->setName('phone_number')
+                        ->setLabel('Phone')
+                        ->setSortable(true)
+                        ->addFilter(
+                            (new FilterConfig)
+                                ->setOperator(FilterConfig::OPERATOR_LIKE)
+                        )
+                    ,
+                    (new FieldConfig)
+                        ->setName('country')
+                        ->setLabel('Country')
+                        ->setSortable(true)
+                    ,
+                    (new FieldConfig)
+                        ->setName('company')
+                        ->setLabel('Company')
+                        ->setSortable(true)
+                        ->addFilter(
+                            (new FilterConfig)
+                                ->setOperator(FilterConfig::OPERATOR_LIKE)
+                        )
+                    ,
+                    (new FieldConfig)
+                        ->setName('birthday')
+                        ->setLabel('Birthday')
+                        ->setSortable(true)
+                    ,
+                    (new FieldConfig)
+                        ->setName('posts_count')
+                        ->setLabel('Posts')
+                        ->setSortable(true)
+                    ,
+                    (new FieldConfig)
+                        ->setName('comments_count')
+                        ->setLabel('Comments')
+                        ->setSortable(true)
+                    , 
+                    (new FieldConfig)
+                        ->setName('id')
+                        ->setLabel('Action')
+                        ->setCallback(function ($val) {
+                            return "<a href='/user/edit/{$val}'><span class='glyphicon glyphicon-user'></span>edit</a>";
+                        })
+                        ->setSortable(true)
+                        ->addFilter(
+                            (new FilterConfig)
+                                ->setOperator(FilterConfig::OPERATOR_LIKE)
+                        ),
+                ])
+                ->setComponents([
+                    (new THead)
+                        ->setComponents([
+                            (new ColumnHeadersRow),
+                            (new FiltersRow)
+                                ->addComponents([
+                                    (new RenderFunc(function () {
+                                        return HTML::style('js/daterangepicker/daterangepicker-bs3.css')
+                                        . HTML::script('js/moment/moment-with-locales.js')
+                                        . HTML::script('js/daterangepicker/daterangepicker.js')
+                                        . "<style>
+                                                .daterangepicker td.available.active,
+                                                .daterangepicker li.active,
+                                                .daterangepicker li:hover {
+                                                    color:black !important;
+                                                    font-weight: bold;
+                                                }
+                                           </style>";
+                                    }))
+                                        ->setRenderSection('filters_row_column_birthday'),
+                                    (new DateRangePicker)
+                                        ->setName('birthday')
+                                        ->setRenderSection('filters_row_column_birthday')
+                                        ->setDefaultValue(['1990-01-01', date('Y-m-d')])
+                                ])
+                            ,
+                            (new OneCellRow)
+                                ->setRenderSection(RenderableRegistry::SECTION_END)
+                                ->setComponents([
+                                    new RecordsPerPage,
+                                    new ColumnsHider,
+                                    (new CsvExport)
+                                        ->setFileName('my_report' . date('Y-m-d'))
+                                    ,
+                                    new ExcelExport(),
+                                    (new HtmlTag)
+                                        ->setContent('<span class="glyphicon glyphicon-refresh"></span> Filter')
+                                        ->setTagName('button')
+                                        ->setRenderSection(RenderableRegistry::SECTION_END)
+                                        ->setAttributes([
+                                            'class' => 'btn btn-success btn-sm'
+                                        ])
+                                ])
+                        ])
+                    ,
+                    (new TFoot)
+                        ->setComponents([
+                            (new TotalsRow(['posts_count', 'comments_count'])),
+                            (new TotalsRow(['posts_count', 'comments_count']))
+                                ->setFieldOperations([
+                                    'posts_count' => TotalsRow::OPERATION_AVG,
+                                    'comments_count' => TotalsRow::OPERATION_AVG,
+                                ])
+                            ,
+                            (new OneCellRow)
+                                ->setComponents([
+                                    new Pager,
+                                    (new HtmlTag)
+                                        ->setAttributes(['class' => 'pull-right'])
+                                        ->addComponent(new ShowingRecords)
+                                    ,
+                                ])
+                        ])
+                    ,
+                ])
+        );
+        $grid = $grid->render();
+        return view('table', compact('grid'));
+    }
+    
+    public function getExample5()
+    {
+        $query = \DB::getDoctrineConnection()->createQueryBuilder();
+        $query
+            ->select([
+                'id',
+                'name',
+                'email',
+                'country',
+                'posts_count'
+            ])
+            ->from('users');
+    
+        $cfg = (new GridConfig())
+            ->setDataProvider(
+                new DbalDataProvider($query)
+            )
+            ->setPageSize(5)
+            ->setColumns([
+                new FieldConfig('id'),
+                new FieldConfig('name'),
+                new FieldConfig('email'),
+                new FieldConfig('country'),
+                new FieldConfig('posts_count'),
+            ]);
+        $grid = new Grid($cfg);
+        $text = '<h1>Grid with DbalDataProvider</h1>';
+        return view('table', compact('grid', 'text'));
+    }
+
+
+}
